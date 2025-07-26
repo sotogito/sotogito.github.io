09f9e557a36af0c85fb1eaf6b1ad3a61fc265a75
sotogito <sotogitoarchive@gmail.com>
2025-07-27 00:09:43 +0900

    update

--- sotogito.github.io/assets/js/script.js
+++ sotogito.github.io/assets/js/script.js
@@ -1,2 +1,16 @@
-// script.js
-// 추후 인터랙션용 JS 코드 작성 
+document.addEventListener('DOMContentLoaded', function () {
+  const img = document.querySelector('#main-img');
+  const verticalText = document.querySelector('#vertical-text');
+
+  img.addEventListener('mouseenter', function () {
+    this.src = 'assets/images/human_conv.png';
+    this.alt = 'human';
+    verticalText.style.opacity = '1';
+  });
+
+  img.addEventListener('mouseleave', function () {
+    this.src = 'assets/images/human.png';
+    this.alt = 'human';
+    verticalText.style.opacity = '0';
+  });
+});
