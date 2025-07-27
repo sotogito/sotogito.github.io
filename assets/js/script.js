9402e0deca9f0e6e06f6ab6284e8f6ae0dd7f1a5
sotogito <sotogitoarchive@gmail.com>
2025-07-27 12:55:46 +0900

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const img = document.querySelector('#main-img');
-  const verticalText = document.querySelector('#vertical-text');
+  const verticalText = document.querySelector('#overlay-vertical');
 
   img.addEventListener('mouseenter', function () {
-    this.src = 'assets/images/human_conv.png';
+    this.src = 'assets/images/fish_4.png';
     this.alt = 'human';
     verticalText.style.opacity = '1';
   });
 
   img.addEventListener('mouseleave', function () {
-    this.src = 'assets/images/human.png';
+    this.src = 'assets/images/fish_3.png';
     this.alt = 'human';
     verticalText.style.opacity = '0';
   });
